061fb1eab95b397bc92f9a763daa5570c48e7650
jldav1d <jldavid019@gmail.com>
2024-05-13 20:46:43 +0800

    [Backend] Dashboard Changes

--- Legal/resources/views/dashboard.blade.php
+++ Legal/resources/views/dashboard.blade.php
@@ -15,8 +15,8 @@
                             <img src="storage/img/briefcase.png" alt="" class="h-14">
                         </div>
                         <div class="stat-title font-semibold">Cases this Month</div>
-                        <div class="stat-value">6</div>
-                        <div class="stat-desc font-semibold">May 2024</div>
+                        <div class="stat-value">{{$case_resolved_count}}</div>
+                        <div class="stat-desc font-semibold">{{$month_and_year}}</div>
                         <div class="stat-actions">
                             <a href="case_matrix"><button class="btn btn-sm btn-outline">Case Matrix</button> </a>
                         </div>
@@ -29,8 +29,8 @@
                             <img src="storage/img/docs.png" alt="" class="h-14">
                         </div>
                         <div class="stat-title font-semibold">Proccessed Documents</div>
-                        <div class="stat-value">789</div>
-                        <div class="stat-desc font-semibold">May 2024</div>
+                        <div class="stat-value">{{$doc_done_count}}</div>
+                        <div class="stat-desc font-semibold">{{$month_and_year}}</div>
                         <div class="stat-actions">
                             <button class="btn btn-sm btn-outline">More</button> 
                         </div>
@@ -43,8 +43,8 @@
                             <img src="storage/img/cert.png" alt="" class="h-14">
                         </div>
                         <div class="stat-title font-semibold">CeNoPAC Generated</div>
-                        <div class="stat-value">14</div>
-                        <div class="stat-desc font-semibold">May 2024</div>
+                        <div class="stat-value">{{$cenopac_generated_count}}</div>
+                        <div class="stat-desc font-semibold">{{$month_and_year}}</div>
                         <div class="stat-actions">
                             <a href="cenopac_record"><button class="btn btn-sm btn-outline">Records</button></a>
                             <a href="cenopac"><button class="btn btn-sm">Generate</button></a>
