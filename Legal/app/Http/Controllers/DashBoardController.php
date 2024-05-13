b1dbb8046bd34947a4b60214881b6b20c7c3b583
jldav1d <jldavid019@gmail.com>
2024-05-13 20:46:43 +0800

    [Backend] Dashboard Changes

--- Legal/app/Http/Controllers/DashBoardController.php
+++ Legal/app/Http/Controllers/DashBoardController.php
@@ -2,11 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\case_matrix;
+use App\Models\cenopac_record;
+use App\Models\document_record;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashBoardController extends Controller
 {
     public function index(){
-        return view('dashboard');
+
+        $currentYear = Carbon::now()->year;
+        $currentMonth = Carbon::now()->month;
+        $month_and_year = Carbon::now()->format('F Y');
+        $case_resolved_count = case_matrix::whereYear('date_resolved',$currentYear)
+            ->whereMonth('date_resolved', $currentMonth)
+            ->where('status', 'Resolved')
+            ->count();
+        // not sure if date_released parameter for date of finished documents
+        $doc_done_count = document_record::whereYear('date_released',$currentYear)
+            ->whereMonth('date_released', $currentMonth)
+            ->where('status', 'Done')
+            ->count();
+        $cenopac_generated_count = cenopac_record::whereYear('date_issued',$currentYear)
+            ->whereMonth('date_issued', $currentMonth)
+            ->count();
+        return view('dashboard', compact('case_resolved_count', 'doc_done_count', 'cenopac_generated_count', 'month_and_year'));
     }
 }
