c02cf4e5c6c29f5a3fb47933d4044af0e687a1a6
Friel05 <masiglajf@gmail.com>
2024-05-16 14:17:23 +0800

    [Backend] Document Board
    - Removed Done X button at
    - Automatically clear Done tasks depending on date of released and current month

--- Legal/app/Livewire/DocumentBoard.php
+++ Legal/app/Livewire/DocumentBoard.php
@@ -3,17 +3,25 @@
 namespace App\Livewire;
 
 use App\Models\document_record;
+use Carbon\Carbon;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class DocumentBoard extends Component
 {
-
+    #[On('rerender')]
     public function render()
     {
+        $currentYear = Carbon::now()->year;
+        $currentMonth = Carbon::now()->month;
+        $dr_done = document_record::whereYear('date_released',$currentYear)
+            ->whereMonth('date_released', $currentMonth)
+            ->where('status', 'Done')
+            ->get();
         return view('livewire.document-board', [
             "dr_to_do" => document_record::where("status","To-Do")->get(),
             "dr_doing" => document_record::where("status","Doing")->get(),
-            "dr_done" => document_record::where("status","Done")->get(),
+            "dr_done" => $dr_done,
         ]
         );
     }
