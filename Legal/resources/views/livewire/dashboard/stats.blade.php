438644fb47ae00334e5d77e1a850ef6fd6daa031
Friel05 <masiglajf@gmail.com>
2024-05-17 18:24:36 +0800

    [BACKEND] Dashboard Updates
    - Introduced 1st Version of Pending Tasks (Subject to Change)

---
+++ Legal/resources/views/livewire/dashboard/stats.blade.php
@@ -0,0 +1,44 @@
+<div class="flex w-full gap-3 justify-items-stretch">
+    <div class="stats shadow w-full bg-info">
+        <div class="stat w-full">
+            <div class="stat-figure text-secondary">
+                <img src="storage/img/briefcase.png" alt="" class="h-14">
+            </div>
+            <div class="stat-title font-semibold">Cases this Month</div>
+            <div class="stat-value">{{$case_resolved_count}}</div>
+            <div class="stat-desc font-semibold">{{$month_and_year}}</div>
+            <div class="stat-actions">
+                <a href="case_matrix"><button class="btn btn-sm btn-outline">Case Matrix</button> </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="stats shadow w-full bg-success">
+        <div class="stat w-full">
+            <div class="stat-figure text-secondary">
+                <img src="storage/img/docs.png" alt="" class="h-14">
+            </div>
+            <div class="stat-title font-semibold">Proccessed Documents</div>
+            <div class="stat-value">{{$doc_done_count}}</div>
+            <div class="stat-desc font-semibold">{{$month_and_year}}</div>
+            <div class="stat-actions">
+                <button class="btn btn-sm btn-outline">More</button> 
+            </div>
+        </div>
+    </div>
+
+    <div class="stats shadow w-full bg-warning">
+        <div class="stat w-full">
+            <div class="stat-figure text-secondary">
+                <img src="storage/img/cert.png" alt="" class="h-14">
+            </div>
+            <div class="stat-title font-semibold">CeNoPAC Generated</div>
+            <div class="stat-value">{{$cenopac_generated_count}}</div>
+            <div class="stat-desc font-semibold">{{$month_and_year}}</div>
+            <div class="stat-actions">
+                <a href="cenopac_record"><button class="btn btn-sm btn-outline">Records</button></a>
+                <a href="cenopac"><button class="btn btn-sm">Generate</button></a>
+            </div>
+        </div>
+    </div>
+</div>
