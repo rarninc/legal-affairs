e6d146450af2c5f1f65b06a5c75edc2702521738
jldav1d <jldavid019@gmail.com>
2024-05-19 14:24:17 +0800

    [Backend] Pending Tasks Changes

--- Legal/app/Livewire/CenopacCertificate.php
+++ Legal/app/Livewire/CenopacCertificate.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\case_matrix;
 use App\Models\cenopac_record;
 use App\Models\cenopac_request;
+use App\Models\pending_task;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Livewire\Component;
 
@@ -42,14 +43,40 @@ class CenopacCertificate extends Component
                 $this->reject = true;
             }
         }
-        if($this->reject){
-            cenopac_request::find($this->id)->update([
+
+        $cr_req = cenopac_request::where('employee_name',$this->id)
+            ->where('position', $this->position)
+            ->where('originating_office', $this->originating_office)
+            ->where('purpose', $this->purpose)
+            ->orWhere('id', $this->id)
+            ->first();
+        if($cr_req == null && $this->reject){
+            cenopac_request::create([
+                'employee_name' => $this->employee_name,
+                'originating_office'=> $this->originating_office,
+                'position' => $this->position,
+                'purpose' => $this->purpose,
+                'date_requested' => $this->date_requested,
                 'status' => 'Denied',
             ]);
             session()->flash('failed', $this->employee_name .' has a record of on-going case/s');
             // $this->reset();  
-            return redirect()->back();  
-        }
+            return redirect()->back();
+        }elseif($this->reject){
+            $cr_req->update([
+                'status' => 'Denied',
+            ]);
+            session()->flash('failed', $this->employee_name .' has a record of on-going case/s');
+            // $this->reset();  
+            return redirect()->back();
+        }elseif($cr_req != null){
+            $cr_req->delete();
+            pending_task::where('record_title', $this->employee_name)
+            ->where('table_name', 'CeNoPac Request')
+            ->orWhere('record_id', (string)$this->id)    
+            ->delete();
+
+        }   
         cenopac_record::create([
             'employee_name' => $this->employee_name,
             'originating_office'=> $this->originating_office,
@@ -63,8 +90,6 @@ class CenopacCertificate extends Component
         $this->date_issued = strtotime($this->date_issued);
         $this->date_issued = date('F d, Y', $this->date_issued);
 
-        cenopac_request::find($this->id)->delete();
-        
         $cenopac = Pdf::loadView('pdf.generate_cenopac_cert', [
             'employee_name' => $this->employee_name,
             'originating_office' => $this->originating_office,
@@ -76,6 +101,7 @@ class CenopacCertificate extends Component
         return response()->streamDownload(function () use ($cenopac) {
             echo $cenopac->stream();
             }, 'cenopac_cert.pdf');
+        
 
     }
 
