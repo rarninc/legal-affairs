980ec8609dd3b9d2c7e2696c3f64a3ea89bfb02c
rarninc <inciongrichardaaron@gmail.com>
2024-05-21 22:57:39 +0800

    [UI] Document Record
    - added filter
    - updated side nav bar

--- Legal/resources/views/livewire/document-view.blade.php
+++ Legal/resources/views/livewire/document-view.blade.php
@@ -23,14 +23,7 @@
             </div>
 
         </div>
-        <!-- Search-->
-        <div>
-            <label class="input input-bordered flex items-center gap-2 w-fit">
-                <input wire:model.live.debounce.300ms = 'search' type="text" class="grow" placeholder="Search"/>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" /></svg>
-            </label>
-        </div>                
-        <!-- Search-->
+        
         <!-- Return -->
         <a href="document_board"> 
             <button class="btn bg-indigo-800 btn-primary text-white items-center">
@@ -44,16 +37,28 @@
     <!-- Title -->
     <!-- Table -->
     <div class="card h-full w-full mt-4 bg-base-100 shadow-xl">
-        <div class="card-body flex flex-col h-1 p-5">         
-            <div class="flex gap-2 h-fit items-center justify-end">
-                <label for="filter_status" class="block text-sm font-medium text-gray-900 w-14">Progress Status:</label>
-                <select wire:model.live="filter_status" id="filter_status" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-40 p-2.5 ">
-                    <option value='' select>All</option>
-                    <option value="To-Do">To-Do</option>
-                    <option value="Doing">Doing</option>
-                    <option value="Done">Done</option>
-                </select>
-            </div>      
+        <div class="card-body flex flex-col h-1 p-5">
+            <div class="mb-2 w-full gap-4 flex items-center">
+                <!-- Search-->
+                <div class="flex-1">
+                    <label class="input input-bordered flex items-center gap-2 w-fit">
+                        <input wire:model.live.debounce.300ms = 'search' type="text" class="grow" placeholder="Search"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" /></svg>
+                    </label>
+                </div>                
+                <!-- Search-->
+                <!-- Filter-->
+                <div class="flex gap-2 h-fit items-center justify-end">
+                    <label for="filter_status" class="block text-sm font-medium text-gray-900 w-30">Progress Status:</label>
+                    <select wire:model.live="filter_status" id="filter_status" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-40 p-2.5 ">
+                        <option value='' select>All</option>
+                        <option value="To-Do">To-Do</option>
+                        <option value="Doing">Doing</option>
+                        <option value="Done">Done</option>
+                    </select>
+                </div>
+                <!-- Filter-->
+            </div>            
             <div class="scroll-container flex-1 overflow-x-auto w-full rounded-lg">
                 <table class="table table-zebra table-pin-rows">
                 <!-- head -->
