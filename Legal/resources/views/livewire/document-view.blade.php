a6bf5b576122a41d2e7662558921f002ea4a2840
rarninc <inciongrichardaaron@gmail.com>
2024-05-21 21:12:30 +0800

    [UI] Document Manager Changes
    - fixed linking

--- Legal/resources/views/livewire/document-view.blade.php
+++ Legal/resources/views/livewire/document-view.blade.php
@@ -8,7 +8,7 @@
             <div class="text-sm breadcrumbs pb-0">
             <ul>
                 <li>
-                <a href="document_board" class="inline-flex gap-2 items-center">
+                <a href="document_record" class="inline-flex gap-2 items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
                     Track Document
                 </a>
