be2d1777c41c2319cad78a763b47c11b15c3c651
Friel05 <masiglajf@gmail.com>
2024-06-03 21:23:33 +0800

    [BACKEND] Fixed Migration Table
    -Testing Migration Table

--- Legal/app/Models/document_record_history.php
+++ Legal/app/Models/document_record_history.php
@@ -9,7 +9,7 @@ class document_record_history extends Model
 {
     use HasFactory;
     protected $table = "legal_document_record_history";
-    protected $fillable = ["id","date_received","date_released","document_title","document_type","from_office","to_office","remarks","tracking_no","version","dt_updated","progress_status","document_status"];
+    protected $fillable = ["id","date_received","date_released","document_title","document_type","from_office","to_office","remarks","tracking_no","version","dt_updated","progress_status","document_status","action"];
     protected $primaryKey = ["id","version"];
     protected $keyType = 'string';
     public $timestamps = false;
